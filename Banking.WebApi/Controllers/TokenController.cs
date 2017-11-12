4606d42eae8281a9d7772ebd489f82e237cb1f8d
Pedro J. Saba <pedrojsaba@gmail.com>
2017-11-11 21:39:50 -0500

    start token

--- Banking.WebApi/Controllers/TokenController.cs
+++ Banking.WebApi/Controllers/TokenController.cs
@@ -5,20 +5,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 
-namespace Banking.MvcService.Controllers
+namespace Banking.WebApi.Controllers
 {
     public class TokenController : ApiController
     {
 
         //http://localhost:10559/api/Accounts/5
         [HttpGet]
-        public Banking.Domain.Model.BankAccount[] FindById(int id)
+        public void FindById(int id)
         {
-            var bankingApplicationService = new BankingApplicationService();
-            Banking.Domain.Model.BankAccount account;
-            account = bankingApplicationService.FindById(id);
-
-            return new[] { account };
+            
         }
 
     }

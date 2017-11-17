294478bcd07c00184c2b1a26d6e9722af7369ddd
Pedro J. Saba <pedrojsaba@gmail.com>
2017-11-16 20:10:35 -0500

    change conex

--- Banking.Repository/Repositories/AdoNET/Functions.cs
+++ Banking.Repository/Repositories/AdoNET/Functions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace Banking.Infrastructure.Repositories
 {
@@ -11,7 +6,7 @@ namespace Banking.Infrastructure.Repositories
     {
        public static string GetConnectionString()
        {
-           return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+           return ConfigurationManager.ConnectionStrings["AuthContext"].ConnectionString;
        }
 
 
